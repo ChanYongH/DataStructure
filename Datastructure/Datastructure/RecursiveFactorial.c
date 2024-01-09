b5efccf787582f6c8b84393eea582d5ee796f56b
Coleyoung <ighong11@gmail.com>
2024-01-09 22:16:29 +0900

    재귀 함수를 활용한 팩토리얼 함수 구현 진행

---
+++ Datastructure/Datastructure/RecursiveFactorial.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int Factorial(int n)
+{
+	if (n == 0)
+		return 1;
+	else
+		return n * Factorial(n - 1);
+}
+
+void main()
+{
+	for(int i = 0; i < 11; i++)
+		printf("%d! = %d \n", i, Factorial(i));
+	// ex) Factorial(5)
+	// 다음 함수는 다음과 같은 순서로 실행된다.
+	// 1. 2 * 1
+	// 2. 3 * 2
+	// 3. 4 * 6
+	// 4. 5 * 24 = 120 Return
+}
